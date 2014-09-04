1a082a7a3dec9b9b144195eb129a2c61cf8138a2
Burawat <Sola_un@hotmail.com>
2014-09-04 15:27:55 +0700

    Listview

---
+++ gen/com/burawat/listview_55410014/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.burawat.listview_55410014;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/burawat/listview_55410014/List_items.java
@@ -0,0 +1,25 @@
+package com.burawat.listview_55410014;
+
+import android.graphics.drawable.*;
+
+public class List_items {
+	private Drawable drawable;
+	private String title;
+
+	public Drawable getDrawable() {
+		return drawable;
+	}
+
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
